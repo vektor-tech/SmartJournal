2eccd22ff5dc42e92b0df1b65d0ef00f387bdf80
muedie <lasafalus@gmail.com>
2018-10-06 23:04:05 -0500

    auth v1

--- models.py
+++ models.py
@@ -33,7 +33,7 @@ class Entry(db.Model):
     time = db.Column(db.DateTime, nullable=False)
 
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    tag_id = db.Column(db.Integer, db.ForeignKey("tab.id"))
+    tag_id = db.Column(db.Integer, db.ForeignKey("tag.id"))
 
     def __init__(self, text, uid, tid):
         self.text = text
