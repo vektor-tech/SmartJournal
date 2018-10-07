e8affa88958d54062cf03c82efd4790695d78fbd
Aabhas Kharel <aabhas.kharel@mavs.uta.edu>
2018-10-06 23:05:42 -0500

    merging

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # hackuta18
 our project for hack uta 2018
+
+Skip #1 if you have virtualenv
+
+
+To run:
+
+1. `pip install -U virtualenv`
+2. `virtualenv venv`
+3. `source venv/bin/activate`
+4. `pip install -r requirements.txt`
+5. `source env.sh`
+6. `python3 application.py`
---
+++ models.py
@@ -0,0 +1,60 @@
+# db models for journal app
+
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+    # user 1:N entry relationship
+    entries = db.relationship("Entry", backref="user", lazy=True)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+    def __str__(self):
+        return f'{self.username}'
+
+class Entry(db.Model):
+    __tablename__ = "entry"
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text)
+    time = db.Column(db.DateTime, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    tag_id = db.Column(db.Integer, db.ForeignKey("tab.id"))
+
+    def __init__(self, text, uid, tid):
+        self.text = text
+        self.user_id = uid
+        self.tag_id = tid
+
+
+class Tag(db.Model):
+    __tablename__ = "tag"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+
+    # tag 1:N entry relationship
+    entries = db.relationship("Entry", backref="tag", lazy=True)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'<Tag {self.name}>'
+
+    def __str__(self):
+        return f'{self.name}'
+        
